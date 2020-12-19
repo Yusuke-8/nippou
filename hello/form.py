194e9de705218545f7907fbb0d9a5f6569e5c130
藤原友輔 <fujiwaratomotasuku@fujiwarayusukes-MacBook-Air.local>
2020-12-20 02:10:26 +0900

    idでレコードを抽出するように作成

--- hello/form.py
+++ hello/form.py
@@ -1,12 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    # name = forms.CharField(label = 'name', widget = forms.TextInput(attrs={'class':'form-control'}))
-    # mail = forms.CharField(label = 'mail', widget = forms.TextInput(attrs={'class':'form-control'}))
-    # age = forms.IntegerField(label = 'age', widget = forms.NumberInput(attrs={'class':'form-control'}))
-    data = [
-        ('one', 'item1'),
-        ('two', 'item2'),
-        ('three', 'item3'),
-    ]
-    choice = forms.ChoiceField(label = 'radio', choices = data, widget = forms.RadioSelect())
+    id = forms.IntegerField(label="ID")
--- hello/views.py
+++ hello/views.py
@@ -2,21 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .form import HelloForm
+from .models import Friend
+
 
 class HelloView(TemplateView):
     def __init__(self):
         self.params = {
             'title': 'Hello',
-            'form': HelloForm(),
-            'result': None
+            'messgae': 'all friends.',
+            'data': Friend.objects.all(),
+            'form': HelloForm()
             }
 
     def get(self, request):
         return render(request, 'hello/index.html', self.params)
 
     def post(self, request):
-        ch = request.POST['choice']
-        self.params['result'] = 'you selected:"' + ch + '".'
+        num = request.POST['id']
+        item = Friend.objects.get(id=num)
+        self.params['data'] = [item]
         self.params['form'] = HelloForm(request.POST)
         return render(request, 'hello/index.html', self.params)
     
