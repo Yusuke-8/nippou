67b09c7ff18874f3c27aa1ccd67e778cc9cfb73e
藤原友輔 <fujiwaratomotasuku@fujiwarayusukes-MacBook-Air.local>
2020-12-24 23:19:49 +0900

    フォーム作成完了

--- sns/forms.py
+++ sns/forms.py
@@ -39,3 +39,21 @@ class GroupSelectForm(forms.Form):
             widget = forms.Select(attr={'class':'form-control'})
         )
 
+class FriendForm(forms.Form):
+    def __init__(self, user, friend=[], vals=[], *args, **kwargs):
+        super(FriendForm, self).__init__.(*args, **kwargs)
+        self.friends['friend'] = forms.MultipleChoiceField(
+            choices=[(item.user, item.user) for item in friends],
+            widget=forms.CheckboxSelecterMultiple(),
+            initial=vals
+        )
+
+class CreateGroupForm(forms.Form):
+    group_name = forms.CharField(max_length=50,widget=forms.TextInput(attr={'class':'form-control'}))
+
+class PostForm(forms.Form):
+    content = forms.CharField(max_length=500, widget=forms.Textarea(attr={'class':'form-control', 'row':2}))
+
+    def __init__(self, user *args, **kwargs):
+        pubcil = User.objects.filtter(username='public').first()
+        self.fields['group'] = forms.ChoiceField(choices[('-','-')] + [(item.title, item.title) for item in Group.objects],widget= forms.Select(attr={'class':'form-control'}))
