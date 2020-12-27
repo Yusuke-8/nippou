4aef0ed4d771d7c78689e1d33b428934c64ab927
藤原友輔 <fujiwaratomotasuku@fujiwarayusukes-MacBook-Air.local>
2020-12-27 22:44:27 +0900

    groupsビュー作成

--- sns/views.py
+++ sns/views.py
@@ -47,3 +47,34 @@ def groups(request):
                 vlist.append(item.user.username)
             groupsform = GroupSelectForm(request, user, request.POST,)
             friendsform = FriendForm(request.user, friendsform=friends, vals=vlist)
+
+    if  request.POST['mode'] == '__friend_form__':
+        sel_group = request.POST['group']
+        group_obj = Group.objects.filter(title=sel_group).first()
+        print(group_obj)
+        sel_fds = request.POST.getlist('friends')
+        sel_users = User.objects.filter(username__in=sel_fds)
+        fds = Friend.objects.filter(owner=request.user).filter(user__in=sel_users)
+        vlist = []
+        for item in fds:
+            item.group = group_obj
+            item.save()
+            vlist.append(item.user.username)
+        message.success(request, ' チェックされたFriendを' + sel_group + 'に登録しました。')
+        groupsform = GroupSelectForm(request.user, {'group':sel_group})
+        friendsform = FriendsForm(request.user, friends=friends, vals=vlist)
+    else:
+        groupsform = GroupSelectForm(request.user)
+        friendsform = FriendForm(request.user, friends=friends, vals=[])
+        sel_group = '-'
+
+    createform = CreateGroupForm()
+    params = {
+        'login_user':request.user,
+        'group_form':groupform,
+        'friends_form':FriendsForm,
+        'create_form':createform,
+        'group':sel_group,
+    }
+    return render(request, 'sns/groups.html', params)
+
